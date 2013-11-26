defc29a23f907eb7f62c8480f0c2164a534ce90c
Gianni <Gianni@XPS>
2013-11-26 15:27:13 +0100

    Create ImportServlet and ImportAction. Also changed some configurations in web,xml

--- WholesalerWebDemo/src/de/htwg_konstanz/ebus/wholesaler/action/ImportAction.java
+++ WholesalerWebDemo/src/de/htwg_konstanz/ebus/wholesaler/action/ImportAction.java
@@ -6,20 +6,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import de.htwg_konstanz.ebus.wholesaler.demo.IAction;
+import de.htwg_konstanz.ebus.wholesaler.demo.LoginBean;
+import de.htwg_konstanz.ebus.wholesaler.demo.util.Constants;
 
-public class ImportAction implements IAction{
+public class ImportAction implements IAction {
 
 	@Override
 	public String execute(HttpServletRequest request,
 			HttpServletResponse response, ArrayList<String> errorList) {
-		// TODO Auto-generated method stub
-		return null;
+		LoginBean loginBean = (LoginBean) request.getSession(true)
+				.getAttribute(Constants.PARAM_LOGIN_BEAN);
+		if (loginBean == null) {
+			loginBean = new LoginBean();
+			request.getSession(true).setAttribute(Constants.PARAM_LOGIN_BEAN,
+					loginBean);
+		}
+		if (loginBean.getRole() == Constants.USER_SUPPLIER) {
+
+			return null;
+
+		} else {
+			errorList
+					.add("Import failed! Be sure that you have the required Role!");
+			return "welcome.jsp";
+		}
 	}
 
 	@Override
 	public boolean accepts(String actionName) {
-		// TODO Auto-generated method stub
-		return false;
+		return actionName.equalsIgnoreCase(Constants.ACTION_IMPORT);
 	}
 
 }
